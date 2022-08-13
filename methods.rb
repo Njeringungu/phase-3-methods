bdd6f34c321fde93f7fc43d1ec710692e0eba8ba
Njeri Ngungu <janetnjerin89@gmail.com>
2022-08-13 18:34:12 +0300

    commit tree 738d83553370d9e3cc9692aa66e1aec3a4f93fa0

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+
+
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+  def greet(name="name")
+    puts "Hello, #{name}!"
+
+  end
+greet "Naureen"
+
+
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add num1,num2
+    return num1 + num2
+end
+sum = add(1,2)
+
+
+def halve number
+   if number.class != Integer
+    return nil
+    
+   else
+    number/2
+    end
+    
